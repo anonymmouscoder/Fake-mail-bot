6aa6a2d0a6ce306855e065a22583b6f65b39b093
Aliou <89993689+anonymmouscoder@users.noreply.github.com>
2022-09-22 09:13:39 +0000

    fichier de configuration

---
+++ config.py
@@ -0,0 +1,2 @@
+API_TOKEN = '5435342774:AAGyUaqUnV2HoCaDHgMNcoCsxcCka76555' #Votre token de bot Telegram depuis @botfather
+admin = 1869143336 #Votre ID telegram pour devenir admin du bot depuis sur le bot Rose en tapant /id
